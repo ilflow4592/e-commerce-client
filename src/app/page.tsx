c1af92cc6f92ac8f787398deca14496b9b5ceca5
ilflow4592 <ilflow4592@gmail.com>
2025-01-10 23:07:07 +0900

    feat : material ui dependency 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+"use server";
+
+import { Box, Button, Typography } from "@mui/material";
+
 const HomePage = () => {
-  return <div>This is Home Page!</div>;
+  return (
+    <div>
+      This is Home Page!
+      <Box sx={{ textAlign: "center", mt: 8 }}>
+        <Typography variant="h4" gutterBottom>
+          Welcome to Material-UI with Next.js!
+        </Typography>
+        <Button variant="contained" color="primary">
+          Click Me
+        </Button>
+      </Box>
+    </div>
+  );
 };
 
 export default HomePage;
