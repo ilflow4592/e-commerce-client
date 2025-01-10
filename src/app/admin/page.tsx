3cb5821161dde0ecd9a46923d88977018816b8e2
ilflow4592 <ilflow4592@gmail.com>
2025-01-10 22:34:10 +0900

    first commit

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,5 @@
+const AdminPage = () => {
+  return <div>This is an Admin Page!</div>;
+};
+
+export default AdminPage;
---
+++ src/app/admin/product/page.tsx
@@ -0,0 +1,5 @@
+const ProductPage = () => {
+  return <div>Hello From Product Page!</div>;
+};
+
+export default ProductPage;
