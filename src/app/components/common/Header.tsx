55b2a617099913243675944198cd0636959228df
ilflow4592 <ilflow4592@gmail.com>
2025-01-21 10:47:46 +0900

    feat : 장바구니 버튼 추가

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -3,7 +3,9 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { styled } from "@mui/material/styles";
-import { Box, Button, Container } from "@mui/material";
+import { Box, Button, Container, IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useState } from "react";
 
 const ShopLinkButton = styled(Button)({
   backgroundColor: "green",
@@ -21,10 +23,26 @@ const AdminLinkButton = styled(Button)({
   },
 });
 
+const CartProductCount = styled("span")({
+  position: "absolute",
+  top: 0,
+  right: 0,
+  backgroundColor: "red",
+  color: "white",
+  width: "20px",
+  height: "20px",
+  borderRadius: "50%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
 const Header = () => {
   const pathname = usePathname();
   const isShopPage = pathname.includes("/shop");
   const isAdminPage = pathname.includes("/admin");
+
+  const [count, setCount] = useState(0);
 
   return (
     <Container maxWidth="xl">
@@ -38,9 +56,24 @@ const Header = () => {
         }}
       >
         {isShopPage && (
-          <Link href="/admin">
-            <AdminLinkButton variant="contained">To Admin</AdminLinkButton>
-          </Link>
+          <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+            <Box sx={{ position: "relative" }}>
+              {/*장바구니에 아이템 */}
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <ShoppingCartIcon />
+              </IconButton>
+              <CartProductCount>{count}</CartProductCount>
+            </Box>
+            <Link href="/admin">
+              <AdminLinkButton variant="contained">To Admin</AdminLinkButton>
+            </Link>
+          </Box>
         )}
 
         {isAdminPage && (
